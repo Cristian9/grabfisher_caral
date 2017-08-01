259c1efc94cd8615efaee14e75c99a19195f5600
Cristian Jhandy TAPIA OLIVERA <ctapia@grupoutp.edu.pe>
2017-08-01 16:52:38 -0500

    Pesquera Caral

--- application/config/config.php
+++ application/config/config.php
@@ -3,6 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
+| Trusted Hosts
+|--------------------------------------------------------------------------
+|
+| In case you want to leave the base_url configuration empty for portability,
+| it involves a risk:
+|
+| http://www.skeletonscribe.net/2013/05/practical-http-host-header-attacks.html	
+|
+| To avoid security issues, you may want to configure a list of "trusted hosts".
+| (for dev and test environments or multi-domain sites)
+|
+| Elements of this list imply you accept any subdomain it has. 
+| For example domain.com also accepts <whatever.>domain.com
+|
+| Array:		array('localhost', 'my-development.com', 'my-production.com', 'subdomain.domain.com')
+|
+*/
+$config['trusted_hosts'] = array('pesquera.herokuapp.com', 'localhost', '127.0.0.1');
+
+/*
+|--------------------------------------------------------------------------
 | Base Site URL
 |--------------------------------------------------------------------------
 |
@@ -23,7 +44,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://pesquera.herokuapp.com/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
