1b55d6a08c3091f2ad8d6ac48a3bb89b6b092183
Cristian Jhandy TAPIA OLIVERA <ctapia@grupoutp.edu.pe>
2017-08-02 14:34:57 -0500

    Pesquera Caral

--- application/views/principal.php
+++ application/views/principal.php
@@ -8,8 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="<?php echo base_url() ?>statics/plugins/bootstrap/bootstrap.css" rel="stylesheet">
         <link href="<?php echo base_url() ?>statics/plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet">
-        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-        <link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
         <link href="<?php echo base_url() ?>statics/plugins/fancybox/jquery.fancybox.css" rel="stylesheet">
         <link href="<?php echo base_url() ?>statics/plugins/fullcalendar/fullcalendar.css" rel="stylesheet">
         <link href="<?php echo base_url() ?>statics/plugins/xcharts/xcharts.min.css" rel="stylesheet">
